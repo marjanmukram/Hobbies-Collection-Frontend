f58e37dd424e2d537713ae25337d4d40cb15f8f3
abdulladone94 <abdulladone94@gmail.com>
2021-12-21 17:43:51 +0530

    UI design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Register</h1>
+      <div className="box">
+        <h1>Register</h1>
+        <label>Username</label>
+        <input type="text" />
+        <label>Password</label>
+        <input type="text" />
+        <button>Register</button>
+      </div>
+      <div className="box">
+        <h1>Login</h1>
+        <label>Username</label>
+        <input type="text" placeholder="Username" />
+        <label>Password</label>
+        <input type="password" placeholder="Password" />
+        <button>Login</button>
+      </div>
     </div>
   );
 }
