7748a304c283fb70ca3797b09ee990716fc5490d
abdulladone94 <abdulladone94@gmail.com>
2021-12-23 10:08:22 +0530

    Home page UI and css

--- src/home.js
+++ src/home.js
@@ -15,16 +15,34 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
+    <div className="homeMain">
       <h1>Loging Success</h1>
-      {allUsers.map((user, id) => {
-        return (
-          <div>
-            <h1>{user.username}</h1>
-            <h1>{user.email}</h1>
-          </div>
-        );
-      })}
+
+      <div className="homeContainers">
+        <h1>Enter your Hobbies </h1>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <button>Submit</button>
+      </div>
+
+      <div className="homeContainers">
+        <h1>Hobbies Collection</h1>
+      </div>
+
+      <div className="homeContainers">
+        <h1>All Users</h1>
+        {allUsers.map((user, id) => {
+          return (
+            <div>
+              <h1>{user.username}</h1>
+              <h1>{user.email}</h1>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
