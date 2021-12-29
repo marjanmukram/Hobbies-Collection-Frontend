588838b61d3761f33150c16dcd1cbce1f97e6e0f
marjanmukram <marjanmkram8@gmail.com>
2021-12-30 04:27:58 +0530

    use object values

--- src/components/enter-hobby.js
+++ src/components/enter-hobby.js
@@ -1,40 +1,20 @@
 import React from "react";
 
-const EnterHobby = ({ hobby, handleChange, submitHobbis }) => {
+const EnterHobby = ({ hobby, handleChange, submitHobbies }) => {
   return (
     <div className='homeContainers'>
       <h1>Enter your Hobbies </h1>
-      <input
-        type='text'
-        name='hobby1'
-        value={hobby.hobby1}
-        onChange={handleChange}
-      />
-      <input
-        type='text'
-        name='hobby2'
-        value={hobby.hobby2}
-        onChange={handleChange}
-      />
-      <input
-        type='text'
-        name='hobby3'
-        value={hobby.hobby3}
-        onChange={handleChange}
-      />
-      <input
-        type='text'
-        name='hobby4'
-        value={hobby.hobby4}
-        onChange={handleChange}
-      />
-      <input
-        type='text'
-        name='hobby5'
-        value={hobby.hobby5}
-        onChange={handleChange}
-      />
-      <button onClick={submitHobbis}>Submit</button>
+      {Object.values(hobby).map((hob, index) => {
+        return (
+          <input
+            type='text'
+            name={`hobby${index}`}
+            value={hob}
+            onChange={handleChange}
+          />
+        );
+      })}
+      <button onClick={submitHobbies}>Submit</button>
     </div>
   );
 };
--- src/components/hobbies-collection.js
+++ src/components/hobbies-collection.js
@@ -8,11 +8,9 @@ const HobbiesCollection = ({ allHobbies }) => {
         {allHobbies.map((hobby) => {
           return (
             <div className='hobbyItem'>
-              <h4>{hobby.hobby1}</h4>
-              <h4>{hobby.hobby2}</h4>
-              <h4>{hobby.hobby3}</h4>
-              <h4>{hobby.hobby4}</h4>
-              <h4>{hobby.hobby5}</h4>
+              {Object.values(hobby).map((hob) => (
+                <h4>{hob}</h4>
+              ))}
             </div>
           );
         })}
